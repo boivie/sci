61aef3a249e04d294fcd02a7aefbb3cbbadff11a
Victor Boivie <victor@boivie.com>
2011-10-30 14:08:12 -0700

    Added first version of SCI (working name)

---
+++ sci/job.py
@@ -0,0 +1,133 @@
+"""
+    sci.job
+    ~~~~~~~
+
+    Simple Continuous Integration
+
+    :copyright: (c) 2011 by Victor Boivie
+    :license: Apache License 2.0
+"""
+import types
+
+
+class Parameter(object):
+    def __init__(self, job, name, description, default,
+                 required = False,
+                 type = "string"):
+        self.job = job
+        self.name = name
+        self.description = description
+        self.required = required
+        self.default = default
+        self.type = type
+
+    def __call__(self):
+        return self.job.PARAMETERS.get(self.name)
+
+
+class Step(object):
+    def __init__(self, name, fun, **kwargs):
+        self.name = name
+        self.fun = fun
+
+    def __call__(self, *args, **kwargs):
+        print("Runing step '%s' with %s" % (self.name, args))
+        return self.fun(*args, **kwargs)
+
+    def run_detached(self, *args, **kwargs):
+        print("Runing DETACHED step '%s' (%s) with %s" % \
+                  (self.name, self.fun, args))
+        return self.fun(*args, **kwargs)
+
+
+class Env(object):
+    def __init__(self, job, name):
+        self.job = job
+        self.name = name
+
+    def set(self, value):
+        self.job.ENV[self.name] = value
+
+    def __call__(self):
+        return self.job.ENV.get(self.name)
+
+
+class Job(object):
+    def __init__(self, import_name):
+        self.import_name = import_name
+        self.named = {}
+        self.ENV = {}
+        self.PARAMETERS = {}
+        self.steps = []
+        self.mainfn = None
+        self.description = ""
+
+    def set_description(self, description):
+        self.description = description
+
+    def parameter(self, name, description = "", default = None,
+                  **kwargs):
+        p = Parameter(self, name, description, default, **kwargs)
+        self.named[name] = p
+        return p
+
+    def env(self, name, **kwargs):
+        e = Env(self, name, **kwargs)
+        self.named[name] = e
+        return e
+
+    def default(self, what, **kwargs):
+        def decorator(f):
+            what.default = f
+            return f
+        return decorator
+
+    def step(self, name, **kwargs):
+        def decorator(f):
+            self.steps.append((name, f))
+            return Step(name, f, **kwargs)
+        return decorator
+
+    def main(self, **kwargs):
+        def decorator(f):
+            self.mainfn = f
+            return f
+        return decorator
+
+    def start(self, **kwargs):
+        print("Starting %s" % (self.import_name))
+        for k in kwargs:
+            self.PARAMETERS[k] = kwargs[k]
+        # Set default parameters
+        for name in self.named:
+            obj = self.named[name]
+            if issubclass(Parameter, obj.__class__):
+                if not name in self.PARAMETERS and obj.default:
+                    if type(obj.default) is types.FunctionType:
+                        self.PARAMETERS[name] = obj.default()
+                    else:
+                        self.PARAMETERS[name] = obj.default
+        self.mainfn()
+        print("All done.")
+
+    def store(self, filename):
+        print("Storing '%s' on the storage node" % filename)
+
+    def get_stored(self, filename):
+        print("Retrieving stored '%s' from the storage node" % filename)
+
+    def run(self, cmd, **kwargs):
+        print("Running CMD '%s'" % cmd)
+
+    def info(self):
+        print("Job %s with main = %s" % (self.import_name, self.mainfn))
+        print("Parameters:")
+        for name in self.named:
+            obj = self.named[name]
+            if issubclass(Parameter, obj.__class__):
+                print("  %s: %s" % (name, obj))
+        print("Environment:")
+        for name in self.named:
+            obj = self.named[name]
+            if issubclass(Env, obj.__class__):
+                print("  %s: %s" % (name, obj))
---
+++ test.py
@@ -0,0 +1,87 @@
+import time, itertools, sys
+from sci import Job
+
+job = Job(__name__)
+
+build_id_prefix = job.parameter("BUILD_ID_PREFIX", "The build ID prefix to use")
+branch = job.parameter("BRANCH", "Manifest branch", required = True)
+manifest_url = job.parameter("MANIFEST_URL", "Manifest URL",
+                       "http://review.company.com/platform/manifest.git")
+manifest_file = job.parameter("MANIFEST_FILE", "Manifest Filename",
+                              default = "default.xml")
+products = job.parameter("PRODUCTS", "Products to build", type = "array")
+variants = job.parameter("VARIANTS", "Variants to build", type = "array",
+                         default = ["eng", "userdebug", "user"])
+
+build_id = job.env("BUILD_ID")
+
+
+@job.default(products)
+def get_products():
+    if "donut" in branch():
+        return ["g1", "emulator"]
+    if "eclair" in branch():
+        return ["droid", "nexus_one", "emulator"]
+    if "gingerbread" in branch():
+        return ["nexus_one", "nexus_s", "emulator"]
+    raise job.error("Don't know which products to build!")
+
+
+@job.default(build_id_prefix)
+def default_build_id_prefix():
+    return branch().upper().replace("-", "_")
+
+
+@job.step("Create Build ID")
+def create_build_id():
+    build_id.set(build_id_prefix() + "_" + time.strftime("%y%m%d_%H%M%S"))
+
+
+@job.step("Create Static Manifest")
+def create_manifest():
+    job.run("repo init -u {{MANIFEST_URL}} -b {{BRANCH}} -m {{MANIFEST_FILE}}")
+    job.run("repo sync --jobs={{JOBS}}", name = "sync")
+    job.run("repo manifest -r -o static_manifest.xml")
+
+    job.store("static_manifest.xml")
+
+
+@job.step("Run single matrix job")
+def run_single_matrix_job(product, variant):
+    result_file = "result-" + build_id() + "-" + \
+        product + "-" + variant + ".zip"
+    job.get_stored("static_manifest.xml")
+    job.run("repo init -u {{MANIFEST_URL}} -b {{BRANCH}}")
+    job.run("cp static_manifest.xml .repo/manifest.xml")
+    job.run("repo sync --jobs={{JOBS}}", name = "sync")
+    job.run("""
+. build/envsetup.sh
+lunch {{product}}-{{variant}}
+make -j{{JOB_CPUS}}""", name = "build")
+    job.run("zip {{result_file}} $OUT/*.img", name = "zip")
+    job.store(result_file)
+
+
+@job.step("Run matrix jobs")
+def run_matrix_jobs():
+    comb = itertools.product(products(), variants())
+    for product, variant in comb:
+        run_single_matrix_job.run_detached(product, variant)
+
+
+@job.step("Send Report")
+def send_report():
+    pass
+
+
+@job.main()
+def run():
+    create_build_id()
+    job.set_description(build_id())
+    create_manifest()
+    run_matrix_jobs()
+    send_report()
+
+
+print(job.info())
+job.start(BRANCH = sys.argv[1])
