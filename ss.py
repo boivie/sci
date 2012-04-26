47c9a4c201777d01ce8e7921169f5d131e4d94a8
Victor Boivie <victor@boivie.com>
2012-04-26 19:54:12 +0200

    ss: migrated to flask (from web.py)

--- ss.py
+++ ss.py
@@ -8,41 +8,31 @@
     :copyright: (c) 2011 by Victor Boivie
     :license: Apache License 2.0
 """
-import socket
-from optparse import OptionParser
-import web, json, os
+import logging
+import os
 
-urls = (
-    '/f/B([0-9a-f]+)/(.+)',     'Transfer',
-    '/list/B([0-9a-f]+).json',  'ListSession')
+from flask import Flask, jsonify, abort, request, send_file, url_for
 
-web.config.debug = False
-app = web.application(urls, globals())
+os.environ['_sci_kind'] = 'ss'
+app = Flask(__name__)
+app.config.from_envvar('SCI_SETTINGS')
 
-
-def jsonify(**kwargs):
-    web.header('Content-Type', 'application/json')
-    return json.dumps(kwargs)
+if app.debug:
+    logging.basicConfig(level=logging.DEBUG)
 
 
-def abort(status, data):
-    print("> %s" % data)
-    raise web.webapi.HTTPError(status = status, data = data)
+def get_spath(build_id):
+    return os.path.join(app.config['SS_PATH'], 'ss-files', build_id)
 
 
-def get_spath(sid):
-    spath = os.path.join(web.config._path, "sessions", sid)
-    return spath
-
-
-def get_fpath(sid, filename):
-    fpath = os.path.join(get_spath(sid), filename)
+def get_fpath(build_id, filename):
+    fpath = os.path.join(get_spath(build_id), filename)
     return fpath
 
 
-def get_url(sid, filename):
-    return "http://%s:%d/f/B%s/%s" % (web.config._hostname, web.config._port,
-                                     sid, filename)
+def get_url(build_id, filename):
+    return url_for('.get_file', build_id=build_id, filename=filename,
+                   _external=True)
 
 
 def rglob(directory):
@@ -55,76 +45,57 @@ def rglob(directory):
     return matches
 
 
-class ListSession:
-    def GET(self, sid):
-        spath = get_spath(sid)
-        matches = []
-        for fname in rglob(spath):
-            matches.append({'filename': fname,
-                            'url': get_url(sid, fname)})
-        return jsonify(files = matches)
+@app.route('/list/<build_id>.json', methods=['GET'])
+def list_session(build_id):
+    spath = get_spath(build_id)
+    matches = []
+    for fname in rglob(spath):
+        matches.append({'filename': fname,
+                        'url': get_url(build_id, fname)})
+    return jsonify(files=matches)
 
 
-class Transfer:
-    def PUT(self, sid, filename):
-        if ".." in filename:
-            abort(403, "Not Allowed")
-        fpath = get_fpath(sid, filename)
-        try:
-            os.makedirs(os.path.dirname(fpath))
-        except OSError:
-            pass
-        try:
-            remaining = int(web.ctx.env.get('CONTENT_LENGTH'))
-        except ValueError:
-            abort(500, "Invalid content length")
+@app.route('/f/<build_id>/<filename>', methods=['PUT'])
+def put_file(build_id, filename):
+    if '..' in filename:
+        abort(403)
+    fpath = get_fpath(build_id, filename)
+    try:
+        os.makedirs(os.path.dirname(fpath))
+    except OSError:
+        pass
+    try:
+        remaining = int(request.headers.get('CONTENT_LENGTH'))
+    except ValueError:
+        abort(500)
 
-        src = web.ctx.env['wsgi.input']
-        with open(fpath, "w") as dst:
-            while True:
-                this_len = min(remaining, 1024 * 1024)
-                if this_len == 0:
-                    break
-                part = src.read(this_len)
-                if len(part) != this_len:
-                    abort(500, "Failed to read data")
-                dst.write(part)
-                remaining -= this_len
+    src = request.stream
+    with open(fpath, 'w') as dst:
+        while True:
+            this_len = min(remaining, 1024 * 1024)
+            if this_len == 0:
+                break
+            part = src.read(this_len)
+            if len(part) != this_len:
+                abort(500)
+            dst.write(part)
+            remaining -= this_len
 
-        if ".." in filename:
-            abort(403, "Not Allowed")
+    if '..' in filename:
+        abort(403)
 
-        return jsonify(status = "ok",
-                       url = get_url(sid, filename))
+    return jsonify(status="ok", url=get_url(build_id, filename))
 
-    def GET(self, sid, filename):
-        try:
-            with open(get_fpath(sid, filename), "rb") as src:
-                while True:
-                    data = src.read(100 * 1024)
-                    if not data:
-                        break
-                    yield data
-        except IOError:
-            abort(404, "Not Found")
+
+@app.route('/f/<build_id>/<filename>', methods=['GET'])
+def get_file(build_id, filename):
+    if '..' in filename or filename.startswith('/'):
+        abort(404)
+    filename = get_fpath(build_id, filename)
+    if not os.path.exists(filename):
+        abort(404)
+    return send_file(filename)
 
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("-p", "--port", dest = "port", default = 6698,
-                      help = "port to use")
-    parser.add_option("--path", dest = "path", default = ".",
-                      help = "path to use")
-    parser.add_option("-u", "--host", dest = "hostname",
-                      help = "override host name")
-
-    (opts, args) = parser.parse_args()
-    if opts.hostname:
-        web.config._hostname = opts.hostname
-    else:
-        web.config._hostname = socket.gethostname()
-    web.config._port = opts.port
-    web.config._path = os.path.realpath(opts.path)
-    print("Using url: http://%s:%d" % (web.config._hostname, web.config._port))
-
-    web.httpserver.runsimple(app.wsgifunc(), ("0.0.0.0", int(opts.port)))
+    app.run(host='0.0.0.0', port=6698, debug=True, threaded=True)
