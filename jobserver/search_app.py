d4fde81c6c856d7ac899e89313ea39499124bcb0
Victor Boivie <victor@boivie.com>
2012-07-07 18:41:14 -0700

    js: Can now search a job/recipe by name

--- jobserver/search_app.py
+++ jobserver/search_app.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, g, request
 
-from jobserver.db import KEY_TAG
+from jobserver.db import KEY_TAG, KEY_JOBS, KEY_RECIPES
 
 app = Blueprint('search', __name__)
 
@@ -10,9 +10,18 @@ def simple():
     q = request.json['q']
     tokens = [word.strip().lower() for word in q.split()]
     tokens = [word for word in tokens if word]
-    # Find jobs
+
+    # Match tags
     matches = g.db.sinter([KEY_TAG % w for w in tokens])
     jobs = [m[1:] for m in matches if m[0] == 'j']
     recipes = [m[1:] for m in matches if m[0] == 'r']
+
+    # Add name
+    for t in tokens:
+        if g.db.sismember(KEY_JOBS, t):
+            jobs.append(t)
+        if g.db.sismember(KEY_RECIPES, t):
+            recipes.append(t)
+
     return jsonify(jobs = jobs,
                    recipes = recipes)
